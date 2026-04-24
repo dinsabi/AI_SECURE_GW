6f761b71f9863ea9877a6dd7d61927c48ef30bf7
Dieudonne Nsabimana <dieudonne.nsabimana@cidns.eu>
2026-04-24 11:09:24 +0000

    add vite config to correct error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
+export default {
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: 'all'
+    allowedHosts: ['frontend', '.github.dev', 'localhost']
   }
-});
+};
